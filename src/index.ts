3dc85abdcd5613472ced7ffa8e8e31320a8c1e6b
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-05-08 22:38:06 -0300

    Fix CORS

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,15 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
+//Setar os headers para resolver erro de CORS
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
 
 app.get("/", async (req, res) => {
   res.json({ message: "Please visit /countries to view all the countries" });
